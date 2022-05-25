4e4c9bd78797c539c2e95662bcb23c29749ab270
razseed <christianmcrae@hotmail.com>
2022-05-25 18:07:14 +0100

    code refactored, senior developer consulted on its development, formatting identified as an issue. make button display to the dom the result

--- Webpage/main.js
+++ Webpage/main.js
@@ -2,11 +2,6 @@
 //console.log the result and find a way to display that in the DOM, also research how to tie max and min values to the userInput for <form>.
 
 
-function displayMessage(){
-    document.getElementById("answer").innerHTML = "show this line";
-}
-let btn = document.getElementById("result");
-btn.addEventListener("click", displayMessage)
 
 
 // document.forms to get HTML Form Value in Javascript
@@ -22,44 +17,72 @@ function inputs() {
     Minimum = parseFloat(Minimum);
     Maximum = parseFloat(Maximum);
     function random(max, min){
-       return Math.floor(Math.random() * (max-min+1)) +min  
+        return Math.floor(Math.random() * (max-min+1)) +min  
     }
     const result = random[0,10);
-    console.log(result);
-}
-//let result = inputs();
-//console.log(result)
+        console.log(result);
+    }
+    //let result = inputs();
+    //console.log(result)
+    */
+   
+   /*
+   function random(max,min){
+       return Math.floor(Math.random()*(max - min +1)) + min
+   }
+   */
+   /*
+   function userinputs() {
+       let Minimum = document.getElementById('Minimum').value;
+       Minimum = parseFloat(Minimum);
+       let Maximum = document.getElementById('Maximum').value;
+       Maximum = parseFloat(Maximum);
+       console.log(Minimum)
+       console.log(Maximum)
+    }
+    */
+   /*
+    let variables = ['',''];
+   console.log(variables)
+   
+   function baseValue () {
+       let Minimum = document.getElementById('Minimum').value;
+       Minimum = parseFloat(Minimum);
+       Minimum = Minimum ? Minimum: 0;
+       console.log(Minimum);
+       variables.push(Minimum);
+    }
+    
+    function maxValue() {
+        let Maximum = document.getElementById('Maximum').value;
+        Maximum = parseFloat(Maximum);
+        Maximum = Maximum ? Maximum : 0;
+        console.log(Maximum);
+        variables.push(Maximum);
+    }
+   function displayMessage(){
+       document.getElementById("answer").innerHTML = "show this line";
+   }
+   let btn = document.getElementById("result");
+   btn.addEventListener("click", displayMessage)
+   
+   let randomValue = variables[Math.floor(Math.random()*variables.length)];
+console.log(randomValue);
 */
-
-/*
-function userinputs() {
-    let Minimum = document.getElementById('Minimum').value;
-    Minimum = parseFloat(Minimum);
-    let Maximum = document.getElementById('Maximum').value;
-    Maximum = parseFloat(Maximum);
-    console.log(Minimum)
-    console.log(Maximum)
-}
-*/
-let variables = ["",""];
-console.log(variables)
-
-function baseValue () {
-    let Minimum = document.getElementById('Minimum').value;
-    Minimum = parseFloat(Minimum);
-    Minimum = Minimum ? Minimum: 0;
-    console.log(Minimum);
-    variables.shift(Minimum);
-}
-
-function maxValue() {
-    let Maximum = document.getElementById('Maximum').value;
-    Maximum = parseFloat(Maximum);
-    Maximum = Maximum ? Maximum : 0;
-    console.log(Maximum);
-    variables.push(Maximum);
-}
-
 function random(max,min){
-    return Math.floor(Math.random()*(max - min +1)) + min
+    return Math.floor(Math.random()*(max - min +1)+min)
+};
+
+function displayRandomValues(){
+    let Minimum = document.getElementById('Minimum').value;
+    let Maximum = document.getElementById('Maximum').value;
+    let min = parseFloat(Minimum);
+    let max = parseFloat(Maximum);
+    console.log(min);
+    console.log(max);
+    const result = random(min,max);
+    console.log(result);
+    document.getElementById('output').value=result;
 }
+let btn = document.getElementById('Result');
+btn.addEventListener("click",displayRandomValues)
